bad4c46379816e24dc39d5a553682dc7af6eb526
Nicolas B S Liell <nicolasbsliell@gmail.com>
2026-07-16 09:59:45 -0300

    fix: corrigindo erro de link de index.hmtl e index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,26 @@
-import { buscarCep } from "../services/viacep.js";
+import { buscarCep } from "../services/viaCep.js";
 
-let form = document.querySelector('#form-cadastro') 
-let campoCep = document.querySelector('#cep') 
+const campoCep = document.querySelector("#cep");
+const btnBuscar = document.querySelector("#btnBuscar");
 
-campoCep.addEventListener('blur', async () => {
-    if(!campoCep.value) return
+async function preencherEndereco() {
+    if (!campoCep.value) return;
 
     try {
-        let endereco = await buscarCep(campoCep.value)
-        document.querySelector('#logradouro').value = endereco.logradouro
-        document.querySelector('#bairro').value = endereco.bairro
-        document.querySelector('#cidade').value = endereco.localidade
-        document.querySelector('#uf').value = endereco.uf
-    } catch (error) {
-        console.error(error.message)
+        const endereco = await buscarCep(campoCep.value);
+
+        document.querySelector("#logradouro").value = endereco.logradouro;
+        document.querySelector("#bairro").value = endereco.bairro;
+        document.querySelector("#cidade").value = endereco.localidade;
+        document.querySelector("#uf").value = endereco.uf;
+
+    } catch (erro) {
+        alert(erro.message);
     }
-})
+}
+
+// Busca ao sair do campo CEP
+campoCep.addEventListener("blur", preencherEndereco);
+
+// Busca ao clicar no botão
+btnBuscar.addEventListener("click", preencherEndereco);
